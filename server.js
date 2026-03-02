7f5cdf8e202cda32f71fde989749b7f4182ad0f0
harshitpandita <harshit.pandita2111@gmail.com>
2026-03-02 13:00:25 +0530

    Trigger push workflow

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const http = require("http");
 
-http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from CI/CD demo!\n");
-}).listen(3000, () => console.log("Server running on port 3000"));
+const VERSION = "v1.0.1"; // change this to trigger GitHub Actions (ex: v1.0.2)
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(`Hello from CI/CD demo! ✅\nVersion: ${VERSION}\n`);
+  })
+  .listen(3000, () => console.log("Server running on port 3000"));
